3fca8da5ee9730389968344a394ce35061eda066
haandol <ldg55d@gmail.com>
2017-05-02 03:55:30 +0900

    add socket.io server

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,37 @@
-class Greeter {
-  constructor(public name: string) { }
-  greet() {
-    return `Hello world ${this.name}!!`;
-  }
-  hugeGreet() {
-    return `HELLO WORLD, ${this.name}!!!!!`;
-  }
-};
+import * as http from "http";
+import * as express from "express";
+import * as socketIO from "socket.io";
 
-const greeter = new Greeter("Vincent");
-console.log(greeter.hugeGreet());
+import { Message } from './domain/message';
+
+class Server {
+
+  public app: express.Express;
+
+  private server: http.Server;
+  private io: any;
+
+  constructor(port: number = 3000) {
+    this.app = express();
+    this.server = http.createServer(this.app);
+    this.io = socketIO(this.server);
+
+    this.init(port);
+  }
+
+  private init(port: number): void {
+    this.server.listen(port, () => {
+      console.log('Listening port %s', port);
+    });
+
+    this.io.on('connect', (socket: any) => {
+      socket.on('message', (message: Message) => {
+        this.io.emit('message', JSON.stringify(message));
+      });
+    });
+  }
+  
+}
+
+const server = new Server();
+export default server.app;
